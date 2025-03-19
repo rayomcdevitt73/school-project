9d422b27acd82085b8be696401cbc82a245772aa
rayomcdevitt73 <rayomcdevitt73@gmail.com>
2025-03-19 04:40:28 +0000

    Add 13.py

---
+++ src/13.py
@@ -0,0 +1,7 @@
+import random
+class RandomPythonCode:
+    def __init__(self):
+        self.my_list = ["a", "b", "c"]
+
+    def get_random_element(self):
+        return random.choice(self.my_list)
