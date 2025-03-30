17639e7217178e670d80df2d0a1374d7f40e7494
rayomcdevitt73 <rayomcdevitt73@gmail.com>
2025-03-30 07:40:22 +0000

    Add 19.py

---
+++ src/19.py
@@ -0,0 +1,9 @@
+import os
+
+def calculate_sum(numbers):
+    total = sum(numbers)
+    return total
+
+if __name__ == "__main__":
+    numbers = [10, 20, 30]
+    print("The sum of the list is:", calculate_sum(numbers))
