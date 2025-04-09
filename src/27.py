a6f2a721671a484c7259e7b453085690cee75d63
rayomcdevitt73 <rayomcdevitt73@gmail.com>
2025-04-09 12:08:28 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,11 @@
+def add_numbers(x, y):
+    """
+    Add two numbers and return the result.
+    >>> add_numbers(10, 20)
+    30
+    """
+    return x + y
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
